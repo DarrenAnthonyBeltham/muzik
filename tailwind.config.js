abaebf5e744cc4ccb570b0d8a69d811ed38668a5
DarrenAnthonyBeltham <darrenanthonybeltham@gmail.com>
2025-07-24 11:19:29 +0700

    Create muzik!

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // scan all React/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
